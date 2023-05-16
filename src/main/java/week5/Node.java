62205500fc58cb4ca32f48906dc54e211af62426
xmxm00 <cmcm0012@gmail.com>
2023-05-16 15:16:53 +0900

    5주차 과제 준비

---
+++ src/main/java/week5/Node.java
@@ -0,0 +1,31 @@
+package week5;
+
+public class Node {
+    private int num;
+    private Node left;
+    private Node right;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
